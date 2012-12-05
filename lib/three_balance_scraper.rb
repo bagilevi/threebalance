c83b2f1b33bff8f15dbbd7a25fc3db9d4b3977df
Levente Bagi <bagilevi@gmail.com>
2012-12-05 22:02:13 +0000

    initial command-line version

---
+++ lib/three_balance_scraper.rb
@@ -0,0 +1,93 @@
+# encoding: utf-8
+require 'mechanize'
+
+String.class_eval do
+  # Removes duplicate whitespaces from within and removes all whitespaces from
+  # the begginning and end.
+  def compact
+    gsub(/\s+/, ' ').strip
+  end
+end
+
+class ThreeBalanceScraper
+  # Input your My3 login details
+  # Return: array with 3 elements:
+  # - balance in pennies
+  # - megabytes remaining
+  # - days to use internet allowance
+  def run(phone_number, password)
+    a = Mechanize.new { |agent| }
+    a.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    page = a.get "https://www.three.co.uk/My3Account/Login"
+    page = a.get "https://www.three.co.uk/My3Account/Login"
+
+    sso_url = page.iframes.first.src
+
+    login_widget = a.get sso_url
+
+    login_form = login_widget.form_with(name: "login_form")
+    login_form.username = phone_number
+    login_form.password = password
+    login_result_page = login_form.click_button
+
+    account_page = a.get login_result_page.links.first.href
+    balance_page = a.click account_page.link_with text: "Your account balance."
+
+    table_nodes = balance_page.search("table.balance")
+
+    internet_table, credit_table = nil, nil
+    table_nodes.map{|node|
+      title = node.search('thead').text.gsub(/\s+/, ' ').strip
+      case title
+      when /Internet/
+        internet_table = InternetBalanceTable.new(node)
+      when /Credit/
+        credit_table = CreditBalanceTable.new(node)
+      end
+    }
+
+    [credit_table.pennies_remaining, internet_table.megabytes_remaining, internet_table.days_remaining]
+  end
+
+  class BalanceTable < Struct.new(:table_node)
+  end
+
+  class InternetBalanceTable < BalanceTable
+    def megabytes_remaining
+      table_node.search('tr.summary td:last').text.strip
+    end
+
+    def last_expiry_date
+      table_node.search('tbody:first tr').to_a[1..-1].map{|row_node|
+        s = row_node.search('td')[1].text.compact
+        if s =~ /(\d{2})\/(\d{2})\/(\d{2})/
+          day, mon, year = $1.to_i, $2.to_i, $3.to_i
+          Date.new(2000 + year, mon, day)
+        else
+          raise "Expiry date not found in #{s.inspect}"
+        end
+      }.max
+    end
+
+    def days_remaining
+      (last_expiry_date - Date.today).to_i
+    end
+  end
+
+  class CreditBalanceTable < BalanceTable
+    def pounds_remaining_string
+      table_node.search('tr.summary td:last').text.strip[/£(\d+\.\d+)/, 1]
+    end
+
+    def pennies_remaining
+      items = pounds_remaining_string.split('.', 2).map(&:to_i)
+      items.first * 100 + items.last
+    end
+  end
+
+end
+
+
+
+
